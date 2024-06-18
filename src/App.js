dfdaff782585c8fb1e1546328cda75cb2bba047c
TristanRoberts <rtristan217@gmail.com>
2024-06-17 23:53:34 -0400

    title change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 
 function App() {
+  const title = "Tristan Roberts Website";
+
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1> {title} </h1>
       </header>
     </div>
   );
