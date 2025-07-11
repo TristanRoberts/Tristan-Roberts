8d2aed38060e5589d72e90d04e23a307a0a1e61f
TristanRoberts <rtristan217@gmail.com>
2025-07-11 19:43:12 -0400

    rename HabitTracker file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HabitTracker from "./pages/HabitTracker.js";
 
 function App() {
   const title = "Tristan Roberts Website";
---
+++ src/pages/HabitTracker.js
@@ -0,0 +1,20 @@
+
+
+function HabitTracker() {
+
+const title = "Habit Tracker";
+
+return (
+    <div classname="HabitTracker">
+        <header classname="HabitTracker-header"> 
+            <h1> {title} </h1>
+        </header>
+    
+    </div>
+
+
+);
+
+}
+
+export default HabitTracker; 
