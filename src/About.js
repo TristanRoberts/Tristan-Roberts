116e8df5ecaa0d87b0d73e7472b296384f6d7d7f
TristanRoberts <rtristan217@gmail.com>
2025-08-12 13:24:32 -0400

    Sticky Header

--- src/About.js
+++ src/About.js
@@ -6,6 +6,21 @@ const About = ()  => {
     return(
         <div>
             <h1>About Me</h1>
+            <h1>About Me</h1>
+            <h1>About Me</h1>
+            <h1>About Me</h1>
+            <h1>About Me</h1>
+            <h1>About Me</h1>
+            <h1>About Me</h1>
+            <h1>About Me</h1>
+            <h1>About Me</h1>
+            <h1>About Me</h1>
+            <h1>About Me</h1>
+            <h1>About Me</h1>
+            <h1>About Me</h1>
+            <h1>About Me</h1>
+            <h1>About Me</h1>
+            <h1>About Me</h1>
         </div>
 
     );
