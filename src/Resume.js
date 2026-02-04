6cd4e40e441fbfafbd9cca3ba0542c7a92be12ac
TristanRoberts <rtristan217@gmail.com>
2026-02-03 21:46:01 -0500

    download resume button functionality

--- src/Resume.js
+++ src/Resume.js
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import React from "react";
-import { Document, Page, pdfjs } from 'react-pdf';
+import { Document, Page, PDFViewer, pdfjs } from 'react-pdf';
 //import { getFilePlugin } from '@react-pdf-viewer/get-file';
 //anchor tag
 import "react-pdf/dist/Page/TextLayer.css"; 
-import pdfResume from "./assets/Tristan_V_Roberts_Resume.pdf"; //this is necessary we cant have direct file path
+import pdfResume from "./assets/Tristan_V_Roberts_Resume.pdf"; //this is necessary we cant have direct file path, importing it allows react to package it, "<Document> component doesnt like direct path to pdf"
+
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
     'pdfjs-dist/build/pdf.worker.min.mjs', 
@@ -34,7 +35,11 @@ const Resume = () => {
     */
 //    const getFilePluginInstance = getFilePlugin();
 //    const { DownloadButton } = getFilePluginInstance; 
-   const [numPages, setNumPages] = useState();
+
+// the "<a href='...'>" is an anchor tag with a button inside it, the href is a path to the public folder, it NEEDS to be the public folder
+
+
+   const [numPages, setNumPages] = useState(); 
    const [pageNumber, setPageNumber] = useState(1); 
 
    const onDocumentLoadSuccess = ({ numPages }) => {
@@ -45,12 +50,14 @@ const Resume = () => {
    return (
     <div>
         <Document file={pdfResume} onLoadSuccess={onDocumentLoadSuccess}>
-            {/* <DownloadButton /> */}
             <Page pageNumber={pageNumber}/>
         </Document>
         <p>
             Page {pageNumber} of {numPages}
         </p>
+        <a href="/Tristan_V_Roberts_Resume.pdf" download="Tristan_V_Roberts_Resume.pdf">
+            <button>Download Resume</button>
+        </a>
     </div>
    );
 }
