1692659408e273803e076ea07cf7d4f009aec47e
TristanRoberts <rtristan217@gmail.com>
2026-06-21 17:27:59 -0400

    removing comments to test merging conflicts

--- src/App.js
+++ src/App.js
@@ -8,13 +8,10 @@ import Resume from "./Resume.js";
 import LinkedInPicture from "./assets/linkedIn_profile_picture.jpg"; 
 
 
-//making a duimmy change
+
 //https://blog.react-pdf.dev/top-6-pdf-viewers-for-reactjs-developers-in-2025
 //https://medium.com/better-programming/how-to-display-download-and-create-pdf-files-in-react-65d2e7ddead7
-//just adding a dummy comment
-//Test1
-//Test2
-//Test3
+
 
 function App() {
   const title = "Hi, I'm Tristan Roberts";
