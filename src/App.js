0d28281251f6bb55d90862fa6edef7535f014278
TristanRoberts <rtristan217@gmail.com>
2026-06-21 11:30:44 -0400

    pushing local branch onto remote repository

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import LinkedInPicture from "./assets/linkedIn_profile_picture.jpg";
 
 //https://blog.react-pdf.dev/top-6-pdf-viewers-for-reactjs-developers-in-2025
 //https://medium.com/better-programming/how-to-display-download-and-create-pdf-files-in-react-65d2e7ddead7
+//just adding a dummy comment
 
 function App() {
   const title = "Hi, I'm Tristan Roberts";
